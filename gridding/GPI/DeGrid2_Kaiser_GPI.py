9515c69950830566916f87b7662d04c3077ecd93
Michael Schar <michael.schar@gmail.com>
2016-03-07 11:38:35 -0500

    Adjust DeGrid2_Kaiser node to changes made in SENSE2:
    1) Add widget with oversampling ratio
    2) Read image domain data instead of k-space data, and zero-pad in image domain according to oversampling ratio, pre-rolloff, inverse FFT, and then grid to trajectory coordinates.

--- gridding/GPI/DeGrid2_Kaiser_GPI.py
+++ gridding/GPI/DeGrid2_Kaiser_GPI.py
@@ -34,9 +34,18 @@ import numpy as np
 
 class ExternalNode(gpi.NodeAPI):
     """DeGridding module for Post-Cartesian Data - works with 2D data.
+        First, data are rolloff corrected with the gridding kernel function.
+        Second, zeroes are added around the image matrix according to the oversampling factor.
+        Third, the data are inverse Fourer transformed and then gridded to the coordinate locations using a Kaiser-Bessel kernel.
+        
+    WIDGET:
+        oversampling ratio: Oversampling and Kaiser-Bessel kernel function according to 
+            Beatty, Philip J., Dwight G. Nishimura, and John M. Pauly. "Rapid gridding
+            reconstruction with a minimal oversampling ratio." Medical Imaging, IEEE
+            Transactions on 24.6 (2005): 799-808.
 
     INPUT:
-        data: gridded k-space
+        data: data in image domain
         coords: nD array sample locations (scaled between -0.5 and 0.5)
     
     OUTPUT:
@@ -44,10 +53,12 @@ class ExternalNode(gpi.NodeAPI):
     """
     def initUI(self):
         # Widgets
+        self.addWidget('DoubleSpinBox', 'oversampling ratio', val=1.375, decimals=3, singlestep=0.125, min=1, max=2, collapsed=True)
+        
 
         # IO Ports
         self.addInPort('data', 'NPYarray', dtype=np.complex64, obligation=gpi.REQUIRED)
-        self.addInPort('coords', 'NPYarray', dtype=np.float32, obligation=gpi.REQUIRED)
+        self.addInPort('coords', 'NPYarray', dtype=[np.float64, np.float32], obligation=gpi.REQUIRED)
         self.addOutPort('out', 'NPYarray', dtype=np.complex64)
 
     def compute(self):
@@ -55,23 +66,46 @@ class ExternalNode(gpi.NodeAPI):
         import numpy as np
         import bni.gridding.grid_kaiser as dg
 
-        crds = self.getData('coords')
-        gdata = self.getData('data')
-         
+        crds = self.getData('coords').astype(np.float32, copy=False)
+        gdata = self.getData('data').astype(np.complex64, copy=False)
+        
+        oversampling_ratio = self.getVal('oversampling ratio')
+        mtx_original = gdata.shape[-1]
+        mtx = np.int(mtx_original * oversampling_ratio)
+        if mtx%2:
+            mtx+=1
+        if oversampling_ratio > 1:
+            mtx_min = np.int((mtx-mtx_original)/2)
+            mtx_max = mtx_min + mtx_original
+        else:
+            mtx_min = 0
+            mtx_max = mtx
+        
         # assume the last dims (i.e. each image) must be degridded independently
         gdata_iter, iter_shape = self.pinch(gdata, stop=-2)
+        if iter_shape == []:
+            iter_shape = [1]
 
         # construct an output array w/ slice dims
         out_shape = iter_shape + list(crds.shape)[:-1]
         out = np.zeros(out_shape, dtype=gdata.dtype)
         out_iter,_ = self.pinch(out, stop=-2)
+        
+        # pre-calculate Kaiser-Bessel kernel
+        kernel_table_size = 800
+        kernel = self.kaiserbessel_kernel( kernel_table_size, oversampling_ratio)
+        
+        # pre-calculate the rolloff for the spatial domain
+        roll = self.rolloff2(mtx, kernel)
 
         # degrid all slices
         for i in range(np.prod(iter_shape)):
-            out_iter[i] = dg.degrid(crds, gdata_iter[i])
+            oversampled_kspace = roll[mtx_min:mtx_max,mtx_min:mtx_max]*gdata_iter[i]
+            oversampled_kspace = self.fft2(oversampled_kspace, dir=1, out_shape=[mtx, mtx])
+            out_iter[i] = dg.degrid(crds, oversampled_kspace, kernel)
 
         # reset array shapes and output
-        self.setData('out', out)
+        self.setData('out', out.squeeze())
 
         return(0)
 
@@ -96,3 +130,69 @@ class ExternalNode(gpi.NodeAPI):
         out_shape = s[:start] + [np.prod(iter_shape)] + s[stop:]
         out.shape = out_shape
         return out, iter_shape
+
+    def fft2(self, data, dir=0, zp=1, out_shape=[], tx_ON=True):
+        # data: np.complex64
+        # dir: int (0 or 1)
+        # zp: float (>1)
+
+        # simplify the fftw wrapper
+        import numpy as np
+        import core.math.fft as corefft
+
+        # generate output dim size array
+        # fortran dimension ordering
+        outdims = list(data.shape)
+        if len(out_shape):
+            outdims = out_shape
+        else:
+            for i in range(len(outdims)):
+                outdims[i] = int(outdims[i]*zp)
+        outdims.reverse()
+        outdims = np.array(outdims, dtype=np.int64)
+
+        # load fft arguments
+        kwargs = {}
+        kwargs['dir'] = dir
+
+        # transform or just zeropad
+        if tx_ON:
+            kwargs['dim1'] = 1
+            kwargs['dim2'] = 1
+        else:
+            kwargs['dim1'] = 0
+            kwargs['dim2'] = 0
+
+        return corefft.fftw(data, outdims, **kwargs)
+
+    def rolloff2(self, mtx_xy, kernel, clamp_min_percent=10):
+        # mtx_xy: int
+        import numpy as np
+        import bni.gridding.grid_kaiser as gd
+
+        # grid one point at k_0
+        dx = dy = 0.0
+        coords = np.array([0,0], dtype='float32')
+        data = np.array([1.0], dtype='complex64')
+        weights = np.array([1.0], dtype='float32')
+        outdim = np.array([mtx_xy, mtx_xy],dtype=np.int64)
+
+        # grid -> fft -> |x|
+        out = np.abs(self.fft2(gd.grid(coords, data, weights, kernel, outdim, dx, dy)))
+
+        # clamp the lowest values to a percentage of the max
+        clamp = out.max() * clamp_min_percent/100.0
+        out[out < clamp] = clamp
+
+        # invert
+        return 1.0/out
+
+    def kaiserbessel_kernel(self, kernel_table_size, oversampling_ratio):
+        #   Generate a Kaiser-Bessel kernel function
+        #   OUTPUT: 1D kernel table for radius squared
+    
+        import bni.gridding.grid_kaiser as dg
+        kernel_dim = np.array([kernel_table_size],dtype=np.int64)
+        return dg.kaiserbessel_kernel(kernel_dim, np.float64(oversampling_ratio))
+
+
